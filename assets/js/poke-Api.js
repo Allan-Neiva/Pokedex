6c0b390f1a0cfcbbf4237370573bdbcdb4e50b79
Allan-Neiva <allan944@hotmail.com>
2023-01-18 11:38:07 -0300

    convertendo Api

--- assets/js/poke-Api.js
+++ assets/js/poke-Api.js
@@ -3,21 +3,38 @@
 //=> airfunctio
 const pokeApi = {}
 
-pokeApi.getpokemonDetail = (pokemon) => {
-return fetch(pokemon.url)
-.then((response) => response.json())
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon =  new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types 
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0,limit = 10) => {
-   
+pokeApi.getpokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+        
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    
+
     return fetch(url)
-    //(then) sucesso da operaçao
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getpokemonDetail))
-    .then((requestePromisse) => Promise.all(requestePromisse))
-    .then((pokemonDatails) => pokemonDatails)
-    .catch((error) => console.error(error))
+        //(then) sucesso da operaçao
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getpokemonDetail))
+        .then((requestePromisse) => Promise.all(requestePromisse))
+        .then((pokemonDatails) => pokemonDatails)
+        
 }
